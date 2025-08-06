0b3640376ea74063c524ad02292f37378fbaa7c3
jongsangkuun <43625848+jongsangkuun@users.noreply.github.com>
2025-08-06 14:14:08 +0900

    feat: 데이터 베이스 연결 함수

---
+++ internal/common/conf/dbConnTest.go
@@ -0,0 +1,20 @@
+package conf
+
+//// 연결 테스트
+//func TestConnection() error {
+//	if DB == nil {
+//		return fmt.Errorf("데이터베이스 연결이 없습니다")
+//	}
+//
+//	sqlDB, err := ConnectPostgreSQL()
+//	if err != nil {
+//		return fmt.Errorf("DB 인스턴스 획득 실패: %v", err)
+//	}
+//
+//	if err := sqlDB.Ping(); err != nil {
+//		return fmt.Errorf("데이터베이스 핑 실패: %v", err)
+//	}
+//
+//	log.Println("데이터베이스 연결 테스트 성공")
+//	return nil
+//}
